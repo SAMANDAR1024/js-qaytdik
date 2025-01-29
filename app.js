4469b0174f37910e716af658e999f631e4f69350
Samandar <zafarovsamandar5444@gmail.com>
2025-01-30 01:04:40 +0500

    finish js ga qaytdikmi diman yanaa aa Ustoz

---
+++ app.js
@@ -0,0 +1,130 @@
+////////////////////////////////////////  1 Misol
+
+// const users = [
+//   { name: "Ali", age: 17 },
+//   { name: "Bobur", age: 22 },
+//   { name: "Sardor", age: 15 },
+//   { name: "Madina", age: 25 },
+// ];
+
+// const new_arr = users.filter((item) => {
+//     return item.age > 18
+// });
+// console.log(new_arr);
+
+////////////////////////////////////2 Misol
+// const users = [
+//     { name: "Ali", age: 20 },
+//     { name: "Bobur", age: 22 },
+//     { name: "Sardor", age: 25 }
+//   ];
+
+//   const new_arr = users.map((item)=>{
+//     return item.name
+//   })
+//   console.log(new_arr);
+
+////////////////////////////////////// 3 Misol
+// const products = [
+//     { name: "Telefon", price: 1200000 },
+//     { name: "Noutbuk", price: 5000000 },
+//     { name: "Dazmol", price: 400000 },
+//     { name: "Sovutgich", price: 2500000 }
+//   ];
+
+//   const new_arr = products.filter((item)=>{
+//     return item.price > 500000
+//   })
+//   console.log(new_arr);
+
+////////////////////////////////// 4 Misol
+
+// const students = [
+//   { name: "Ali", grade: 4 },
+//   { name: "Bobur", grade: 3 },
+//   { name: "Madina", grade: 5 },
+// ];
+
+// const new_arr = students.map((item) => ({
+//   name: item.name,
+//   grade: item.grade + 1,
+// }));
+// console.log(new_arr);
+
+/////////////////////////////// 5 Misol
+
+// const names = ["ali", "bobur", "madina", "sardor"];
+
+// const new_arr = names.map(item => item.toUpperCase())
+
+// console.log(new_arr);
+
+////////////////////////////// 6 Misol
+
+// const products = [
+//   { name: "Telefon", price: 1200000 },
+//   { name: "Noutbuk", price: 5000000 },
+//   { name: "Dazmol", price: 400000 },
+// ];
+
+// const chegirma = products.map((item) => ({
+//   name: item.name,
+//   price: item.price * 0.9,
+// }));
+
+// console.log(chegirma);
+
+///////////////////////////////// 7 Misol
+
+// const users = [
+//   { name: "Ali", age: 20 },
+//   { name: "Bobur", age: 22 },
+//   { name: "Anvar", age: 25 },
+//   { name: "Madina", age: 18 },
+// ];
+
+// const new_arr = users.filter((item) => {
+//     return item.name.includes('A')
+// });
+// console.log(new_arr);
+
+////////////////////////////////// 8 Misol
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const new_arr = numbers.filter((item) => {
+//   return item % 2 === 1;
+// });
+// console.log(new_arr);
+
+/////////////////////////////// 9 Misol
+// const workers = [
+//     { name: "Ali", salary: 2000000 },
+//     { name: "Madina", salary: 4000000 },
+//     { name: "Sardor", salary: 2500000 }
+//   ];
+
+//   const new_arr = workers.map(item=>{
+//     if (item.salary < 3000000) {
+//         item.positon = 'Qulvatcha'
+//     }
+//     return item
+//   })
+//   console.log( new_arr);
+
+////////////////////////////////// 10 Misol
+///////// 15 % CHEGRMADA BOGANLANI CHIQARDM FQT USTOZ
+// const products = [
+//   { name: "Telefon", price: 1200000 },
+//   { name: "Noutbuk", price: 5000000 },
+//   { name: "Dazmol", price: 400000 },
+//   { name: "Sovutgich", price: 2500000 },
+// ];
+
+// const new_arr = products
+//   .filter((item) => item.price > 500000 )
+//   .map((item) => ({
+//     name: item.name,
+//     price: item.price * 0.85,
+//   }));
+
+// console.log(new_arr);
